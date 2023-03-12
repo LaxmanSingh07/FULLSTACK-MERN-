34109e1fb7eadacd2f67a2b1f665d93bf32269be
LAXMAN <laxsingh9659@gmail.com>
2023-03-12 21:00:22 +0530

    ""

--- js/DOM03/index.js
+++ js/DOM03/index.js
@@ -1,33 +1,33 @@
-//adding 100paragraphs to the DOM
+// //adding 100paragraphs to the DOM
 
-//standard way to measure the time taken by the code to execute
-//performance.now() ---> timespan in milliseconds
-
-
-//it will take more time to execute as compared to the below code
-let t1=performance.now()
-for(let i=1;i<=100;i++){
-    let newElement=document.createElement('p')
-    newElement.textContent=`this is the paragraph number ${i}`
-    document.body.appendChild(newElement)
-}
-let t2=performance.now()
-console.log("tgis tool "+(t2-t1)+" milliseconds");
-//optimsing a bit 
+// //standard way to measure the time taken by the code to execute
+// //performance.now() ---> timespan in milliseconds
 
 
-//it will take less time to execute as compared to the above code
-t1=performance.now()
-let myDiv=document.createElement('div')
-for(let i=1;i<=100;i++){
-    let newElement=document.createElement('p')
-    newElement.textContent=`this is the paragraph number ${i}`
-    myDiv.appendChild(newElement)
-}
-t2=performance.now()
-console.log("tgis tool "+(t2-t1)+" milliseconds");
+// //it will take more time to execute as compared to the below code
+// let t1=performance.now()
+// for(let i=1;i<=100;i++){
+//     let newElement=document.createElement('p')
+//     newElement.textContent=`this is the paragraph number ${i}`
+//     document.body.appendChild(newElement)
+// }
+// let t2=performance.now()
+// console.log("tgis tool "+(t2-t1)+" milliseconds");
+// //optimsing a bit 
 
-document.body.appendChild(myDiv)
+
+// //it will take less time to execute as compared to the above code
+// t1=performance.now()
+// let myDiv=document.createElement('div')
+// for(let i=1;i<=100;i++){
+//     let newElement=document.createElement('p')
+//     newElement.textContent=`this is the paragraph number ${i}`
+//     myDiv.appendChild(newElement)
+// }
+// t2=performance.now()
+// console.log("tgis tool "+(t2-t1)+" milliseconds");
+
+// document.body.appendChild(myDiv)
 
 //reflow and repaint 
 
@@ -44,3 +44,73 @@ document.body.appendChild(myDiv)
 //it is used to store the list of nodes
 //DocumentFragment is not a part of the DOM
 //if we are adding some node inside the DocumentFragment then it will not cause the reflow and repaint
+
+
+//when you are trying to update dom many times 
+
+// let fragment=document.createDocumentFragment();
+// for(let i=1;i<=100;i++){
+//     let newElement=document.createElement('p');
+//     newElement.textContent=`this is the paragraph number ${i}`;
+//     fragment.appendChild(newElement);
+// }
+// document.body.appendChild(fragment); //1 reflow and 1 repaint
+
+//SINGLE-THREDING
+
+//one command at a time (js ---> single-threded language)
+//The CALL STACK
+
+
+function addParagraph(){
+    let para=document.createElement('p');
+    para.textContent="this is a paragraph1";
+    document.body.appendChild(para);
+}
+function appendNewMessage(){
+  let para=document.createElement('p');
+  para.textContent="this is a paragraph2";
+  document.body.appendChild(para);
+}
+addParagraph();
+appendNewMessage();
+
+
+// observation
+// 1. run to completion
+//2. doesn't execute the multiple lines of code at the same time
+//3. it executes the code line by line
+//4. it executes the code in the order in which it is written
+
+
+//CALL STACK
+
+//global execution context --->    
+
+
+//what is event loops 
+
+//event loop is a mechanism which is used to execute the code asynchronously
+// Philip Roberts
+
+//Asynchronous code ---> is executed and handle by the event loop
+
+//CODE MOVES FORM QUEUE TO THE CALL STACK 
+
+
+//setTimeout is a function which is used to execute the code after the specified time
+
+setTimeout(function(){
+    console.log("this is the first message");
+},1000); 
+
+//there is no gurentee that the code will be executed after the specified time
+//time can be more than the specified time
+
+//Queue will exectute the quque after the call stack is empty
+
+
+setTimeout(()=>{
+    console.log("this is the second message");
+},0)
+//this will be executed first as the call stack is empty
---
+++ js/DOM04/index.js
@@ -0,0 +1,53 @@
+//Asynchronous code in javascript 
+//event loop 
+
+
+//this is the example of asynchronous code
+// function sync(){
+//     console.log("sync function");
+// }
+
+// sync();
+// console.log('second message');
+
+
+//example of the synchronous code
+
+// setTimeout(function(){
+//     console.log("this is the first message");
+// }, 0);
+
+// console.log("This is hello")
+
+// console.log("This is another hello")
+
+
+// API ----> APPLICATION PROGRAMMING INTERFACE
+// API is a set of rules that allow two software programs to communicate with each other
+
+//what is the meaning of the interface
+//interface is a set of rules that allow two software programs to communicate with each other
+
+
+
+
+//promise in javascript
+//promise is a object that may produce a single value some time in the future either a resolved value or a reason that it's not resolved
+//promise is a object that may produce a single value some time in the future either a resolved value or a reason that it's not resolved
+
+//PROMISE MAY BE IN THREE STATE
+//1. PENDING
+//2. FULFILLED
+//3. REJECTED
+
+//promise is used for the parallel execution of the code
+
+
+// let promise = new Promise(function(resolve, reject) {
+//   setTimeout(() => resolve("done!"), 1000);
+
+// });
+
+
+// console.log('first message');
+
